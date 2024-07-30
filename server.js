9e2665423c905ec602aadea5230fe16237b7848b
Xavier <xavier3435@icloud.com>
2024-07-30 10:13:07 -0400

    90%

--- server.js
+++ server.js
@@ -13,20 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-
-
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
-
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
-
-
 
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {

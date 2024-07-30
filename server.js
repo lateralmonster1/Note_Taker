c47e28d80c5b065be4992d95786b2b7b5de39e56
Xavier <xavier3435@icloud.com>
2024-07-30 10:01:18 -0400

    2.0

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware to parse JSON and urlencoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the "public" directory
+
 app.use(express.static('public'));
 
-// HTML Routes
 
-// GET /notes should return the notes.html file.
+
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// GET * should return the index.html file.
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// API Routes
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
@@ -38,7 +38,7 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-// POST /api/notes should receive a new note, save it, and return the new note to the client.
+
 app.post('/api/notes', (req, res) => {
     const newNote = { id: uuidv4(), ...req.body };
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -58,7 +58,7 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
-// DELETE /api/notes/:id should delete the note with the given id.
+
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -78,7 +78,7 @@ app.delete('/api/notes/:id', (req, res) => {
     });
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });

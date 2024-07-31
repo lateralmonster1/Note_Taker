6c8b1a86b59b766fc57a7247e36b30616eb382e2
Xavier <xavier3435@icloud.com>
2024-07-30 22:05:13 -0400

    notes link fix

--- public/js/index.js
+++ public/js/index.js
@@ -83,9 +83,9 @@ const handleNoteSave = () => {
   });
 };
 
-// Delete the clicked note
+
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
+  
   e.stopPropagation();
 
   const note = e.target;
